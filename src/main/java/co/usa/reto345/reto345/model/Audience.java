b1bd846db6e9f921a382b24c72a90edc3663154b
johnfer <john.fernandz.mt@correo.usa.edu.co>
2021-11-02 15:28:32 -0500

    algunos cambios

--- src/main/java/co/usa/reto345/reto345/model/Audience.java
+++ src/main/java/co/usa/reto345/reto345/model/Audience.java
@@ -3,7 +3,15 @@ package co.usa.reto345.reto345.model;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -13,23 +21,47 @@ public class Audience implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    /**
+     * Se crean la columna id
+     */
     private Integer id;
+     /**
+     * Se crean la columna name
+     */
     private String name;
+     /**
+     * Se crean la columna owner
+     */
     private String owner;
+     /**
+     * Se crean la columna capacity
+     */
     private Integer capacity;
+     /**
+     * Se crean la columna description
+     */
     private String description;
 
     @ManyToOne
     @JoinColumn(name="categoryId")
     @JsonIgnoreProperties("audiences")
+    /**
+     *  para llamar los datos de category
+     */
     private Category category;
 
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "audience")
     @JsonIgnoreProperties({"audience", "client"})
+    /**
+     * para llamar los datos de message
+     */
     private List<Message> messages;
 
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "audience")
     @JsonIgnoreProperties({"audience", "client"})
+    /**
+     * para llamar los dtaos de reservation
+     */
     private List<Reservation> reservations;
 
     public Integer getId() {
@@ -94,9 +126,7 @@ public class Audience implements Serializable {
 
     public void setReservations(List<Reservation> reservations) {
         this.reservations = reservations;
-    }
-
-    
+    }   
     
 
 }
--- src/main/java/co/usa/reto345/reto345/web/ReservationController.java
+++ src/main/java/co/usa/reto345/reto345/web/ReservationController.java
@@ -3,6 +3,7 @@ package co.usa.reto345.reto345.web;
 import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
